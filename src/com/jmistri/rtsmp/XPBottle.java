c596845e611c08f9d555d89d2d744d3c8de49282
Kyle <kyle.marino22@gmail.com>
2018-07-17 15:15:12 -0700

    Finishing XPBottle Plugin

--- src/com/jmistri/rtsmp/XPBottle.java
+++ src/com/jmistri/rtsmp/XPBottle.java
@@ -1,21 +1,12 @@
 package com.jmistri.rtsmp;
 
-import de.tr7zw.itemnbtapi.NBTItem;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
-import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
-
-import java.util.Vector;
-
-import static org.bukkit.inventory.EquipmentSlot.HAND;
 
 public class XPBottle implements Listener {
 
@@ -23,63 +14,46 @@ public class XPBottle implements Listener {
     public void onPlayerInteract(PlayerInteractEvent event){
 
         ItemStack items = event.getItem();
-        Player player = Bukkit.getPlayer("kylemarino22");
+        Player player = event.getPlayer();
 
         if(items.getType().equals(Material.GLASS_BOTTLE)
                 && event.getAction().equals(Action.RIGHT_CLICK_BLOCK)
                 || event.getAction().equals(Action.RIGHT_CLICK_AIR)){
 
-            if(player.getTotalExperience() > 7){
+            //If the player has more than 8 xp
+            if(player.getTotalExperience() > 8){
                 int currentXp = player.getTotalExperience();
-                player.setTotalExperience(currentXp - 7);
-//                player.setLevel(15);
-                player.sendMessage("Level: " +  player.getLevel());
-                player.sendMessage("Totalxp: " + getTotalExperience(player));
-
-
-
+                setActualExp(player,currentXp - 8);
 
                 ItemStack xpBottle = new ItemStack(Material.EXP_BOTTLE);
 
                 player.getInventory().addItem(xpBottle);
-                player.getInventory().remove(new ItemStack(Material.EXP_BOTTLE,1));
+                player.getInventory().remove(new ItemStack(Material.GLASS_BOTTLE,1));
                 player.updateInventory();
 
             }
-//            player.sendMessage("xP: " + items);
 
         }
-        player.sendMessage("Item: " + items);
-        player.sendMessage("Item: " + event.getAction());
-
 
     }
 
-    private float getTotalExperience(Player Who) {
-        int level = Who.getLevel();
-        float progress = Who.getExp();
-        float totalExp = 0;
+    private void setActualExp(Player p, int Exp){
 
-        if(level < 15){
-            totalExp += level * level + 6*level;
-        }
-        else if(level < 30){
-            totalExp += 2.5 * level * level - 40.5 * level + 360;
-        }
-        else {
-            totalExp += 4.5*level*level - 162.5 * level + 2220;
-        }
-        progress = (level >= 15 ) ? ((level >= 30) ? progress * (112 + (level - 30) * 9) : progress * (37 + (level - 15) * 5)) : progress * (7 + level * 2);
+        //Reset player's xp
+        p.setTotalExperience(Exp);
+        p.setLevel(0);
+        p.setExp(0);
 
-        return totalExp + progress;
+        //For each level, subtract xp and add a level
+        for(;Exp > p.getExpToLevel();)
+        {
+            Exp -= p.getExpToLevel();
+            p.setLevel(p.getLevel()+1);
+        }
+
+        //Add remaining xp
+        float xp = (float)Exp / (float)p.getExpToLevel();
+        p.setExp(xp);
     }
 
-    private void setTotalExperience(Player who, float Exp){
-
-        if(Exp < 37){
-//            int level =
-        }
-
-
-    }
 }
